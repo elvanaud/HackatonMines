2a241a6872cd64e3ae394ac7f1378f9bddceae7d
NicolasMatte <nicolas.matte@etu.minesparis.psl.eu>
2023-01-27 13:15:20 +0100

    void

--- include/ennemi.h
+++ include/ennemi.h
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <vector>
 
+struct Vect2{ // Volé au main mais amélioré
+    int x;
+    int y;
+    bool est_egal(Vect2 v)
+    {
+        return (this->x == v.x) && (this->y == v.y);
+    }
+};
 
 class Ennemi
 {
@@ -11,7 +19,7 @@ class Ennemi
     public :
 
 
-
+    Vect2 pos;
 };
 
 class Zombie
