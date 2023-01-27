8774edcbc3ed9e7e3287e664db34bf4d8c033072
RaphaelTRIBOT <raphaelemilien.tribot@gmail.com>
2023-01-27 12:12:02 +0100

    score

--- src/score.cpp
+++ src/score.cpp
@@ -1,19 +1,20 @@
 #include <vector>
 #include <fstream>
-#include <ostream>
-#include "../include/items.h"
+#include <iostream>
+#include "include/items.h"
 
+Inventaire inventaire;
 
 std::string printInventaire()
 {
-    unsigned int money = inventaire.gold();
-    unsigned int pv = inventaire.life();  // faire attention que les méthodes sont bien les bonnes
-    unsigned int life_max = inventaire.life_max();
-    std::string weapon = inventaire.weapon();
-    std::string armor = inventaire.armor();
-    unsigned int potion_vie = inventaire.potion_vie();
-    unsigned int potion_force = inventaire.potion_force();
-    std::string strength = inventaire.strength();    
+    unsigned int money = inventaire.gold;
+    unsigned int pv = inventaire.life;  // faire attention que les méthodes sont bien les bonnes
+    unsigned int life_max = inventaire.life_max;
+    std::string weapon = inventaire.weapon;
+    std::string armor = inventaire.armor;
+    unsigned int potion_vie = inventaire.potions_vie;
+    unsigned int potion_force = inventaire.potions_force;
+    //std::string strength = inventaire.strength;    
 
     std::string LifeBar = "";
     for(i=0;i<=pv;i++)
